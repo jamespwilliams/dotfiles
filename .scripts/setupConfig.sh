ed6a2779dddc5ba3e8e1bf279c091319e663c45d
James Williams <jamesphwilliams@gmail.com>
2019-05-06 11:07:20 +0100

    Add source .bash_profile line

--- .scripts/setupConfig.sh
+++ .scripts/setupConfig.sh
@@ -2,3 +2,4 @@ git clone --separate-git-dir=$HOME/.myconf https://github.com/jamespwilliams/dot
 rm -r ~/myconf-tmp/
 /usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME reset --hard HEAD
 /usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME config status.showUntrackedFiles no
+source $HOME/.bash_profile 
