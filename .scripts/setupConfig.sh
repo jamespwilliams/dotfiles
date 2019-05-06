ca51b013fb0a5fce71794788df793c149a08377c
James Williams <jamesphwilliams@gmail.com>
2019-05-06 10:36:47 +0100

    Add setup script to pull dotfiles

---
+++ .scripts/setupConfig.sh
@@ -0,0 +1,4 @@
+git clone --separate-git-dir=$HOME/.myconf https://github.com/jamespwilliams/dotfiles.git $HOME/myconf-tmp
+cp ~/myconf-tmp/.gitmodules ~  # If you use Git submodules
+rm -r ~/myconf-tmp/
+alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
